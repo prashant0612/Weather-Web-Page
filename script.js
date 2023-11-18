c20490e3037f694d9320066e1027143ad1d1a1df
kritirajmaharana <124510753+kritirajmaharana@users.noreply.github.com>
2023-11-18 14:57:12 +0530

    update js location lon lat

--- script.js
+++ script.js
@@ -15,26 +15,29 @@ function handleInput(){
       })
       .then(data => {
         console.log(data); // Log the result to see its structure
-          const city = data.city.name;
-          const cityElement = document.querySelector('.location');
-          cityElement.innerHTML = city;
+          const city = data.city;// whole city details ->lat,lon,timezone,sunrise,sunset
+          const datalist = data.list; //whole whether report list thik hai
 
+          
           var currentDate = new Date();
           var hours = currentDate.getHours();
           var minutes = currentDate.getMinutes();
-
-
-
-          tempCelcious = Math.floor(temp - 273.15);
+          
+          
+          const temp = datalist[0].main.temp; //fatching current temp of the city
+          tempCelcious = Math.floor(temp - 273.15);// converting temp - kelvin to celcius 
 
           const temperature = document.querySelector('.deg');
           const timeLonLat = document.querySelector('.info p')
           const main1deg = document.querySelector('.main1 .main1deg')
+          const cityElement = document.querySelector('.location');
 
-
+          
+          
+          cityElement.innerHTML = `<h2>${city.name}</h2>`;
           temperature.innerHTML = `${tempCelcious}<span class="cel">&deg;c</span>`;
-          timeLonLat.innerHTML = `<p>${hours}:${minutes} | H:${lat.toFixed(2)}&deg; L:${lon.toFixed(2)}&deg;</p>`
-          main1deg.innerHTML = `${tempCelcious} <span class="cel">&deg;c</span>`
+          timeLonLat.innerHTML = `<p>${hours}:${minutes} | H:${city.coord.lat.toFixed(2)}&deg; L:${city.coord.lon.toFixed(2)}&deg;</p>`;
+          main1deg.innerHTML = `${tempCelcious} <span class="cel">&deg;c</span>`;
 
          
       })
